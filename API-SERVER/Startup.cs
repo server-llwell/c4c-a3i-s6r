a892687db8cf0762556a2fbbb70509c89b4da500
mitoriko <mitoriko@icloud.com>
2018-06-05 15:00:45 +0800

    init

---
+++ API-SERVER/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace API_SERVER
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddCors(options => 
+                             options.AddPolicy("AllowSameDomain",builder => 
+                                               builder.AllowAnyOrigin()
+                                               .AllowAnyHeader()
+                                               .AllowAnyMethod()
+                                               .WithExposedHeaders(new string[]{"code", "msg"})
+                                               .AllowCredentials()));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+            app.UseCors("AllowSameDomain");
+        }
+    }
+}
